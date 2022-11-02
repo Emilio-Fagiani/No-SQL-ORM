3b906a68e3d8fdcc4a8440a4793c30999758fe86
Emilio <emi00@gmail.com>
2022-11-02 16:52:03 +0100

    feat: First commit

---
+++ src/main/java/it/develhope/dbnoSQLOrm/DbNoSqlOrmApplication.java
@@ -0,0 +1,13 @@
+package it.develhope.dbnoSQLOrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbNoSqlOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbNoSqlOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/develhope/dbnoSQLOrm/DbNoSqlOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.dbnoSQLOrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbNoSqlOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
